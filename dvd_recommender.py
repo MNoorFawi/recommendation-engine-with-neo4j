62141fedd744108d38c18fb3eb95f5e3e0973889
Muhammad N. Fawi <m.noor.fawi@gmail.com>
2020-03-04 01:08:37 +0400

    update for py2neo changes

--- dvd_recommender.py
+++ dvd_recommender.py
@@ -1,18 +1,18 @@
 ## Our Recommender System Script (dvd_recommender.py)
 import sys
 from pprint import pprint
-from py2neo import authenticate, Graph
+from py2neo import Graph
 
 cid = sys.argv[1:]
 
-authenticate("localhost:7474", "username", "password") 
-g = Graph("http://localhost:7474/db/data/")
+#authenticate("localhost:7474", "username", "password") 
+g = Graph("http://localhost:7474/db/data/", password = "password")
 
 def cf_recommender(graph, cid, nearest_neighbors, num_recommendations):
 
     query = """
            MATCH (c1:Customer)-[:RENTED]->(f:Film)<-[:RENTED]-(c2:Customer)
-           WHERE c1 <> c2 AND c1.customerID = {cid}
+           WHERE c1 <> c2 AND c1.customerID = $cid
            WITH c1, c2, COUNT(DISTINCT f) as intersection
            
            MATCH (c:Customer)-[:RENTED]->(f:Film)
@@ -23,8 +23,8 @@ def cf_recommender(graph, cid, nearest_neighbors, num_recommendations):
                 (intersection * 1.0 / union) as jaccard_index
 
            ORDER BY jaccard_index DESC, c2.customerID
-           WITH c1, COLLECT(c2)[0..{k}] as neighbors
-           WHERE SIZE(neighbors) = {k}                                              
+           WITH c1, COLLECT(c2)[0..$k] as neighbors
+           WHERE SIZE(neighbors) = $k                                              
            UNWIND neighbors as neighbor
            WITH c1, neighbor
 
@@ -33,13 +33,13 @@ def cf_recommender(graph, cid, nearest_neighbors, num_recommendations):
            WITH c1, f, COUNT(DISTINCT neighbor) as countnns
            ORDER BY c1.customerID, countnns DESC                            
            RETURN c1.customerID as customer, 
-                  COLLECT(f.Title)[0..{n}] as recommendations      
+                  COLLECT(f.Title)[0..$n] as recommendations      
            """
 
     recommendations = {}
     # cid = [str(c) for c in cid]
     for c in cid:
-        for i in graph.cypher.execute(query, cid = c, k = nearest_neighbors, n = num_recommendations):
+        for i in graph.run(query, cid = c, k = nearest_neighbors, n = num_recommendations).data():
             recommendations[i[0]] = i[1]
     return recommendations
 
